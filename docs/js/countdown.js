a8f2556cafd8478c773c10db723d963ad108f909
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-05-05 11:32:59 -0500

    update time

--- docs/js/countdown.js
+++ docs/js/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
 	year: 2022, // required
 	month: 5, // required
-	day: 8, // required
-	hours: 7, // Default is 0 [0-23] integer
+	day: 15, // required
+	hours: 12, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
@@ -12,11 +12,11 @@ simplyCountdown('#cuenta', {
 		seconds: 'Second',
 		pluralLetter: 's'
 	},
-	plural: false, //use plurals
+	plural: true, //use plurals
 	inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
 	inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
 	// in case of inline set to false
-	enableUtc: true, //Use UTC as default
+	enableUtc: false, //Use UTC as default
 	onEnd: function() {
 		document.getElementById('portada').classList.add('oculta');
 		return; 
